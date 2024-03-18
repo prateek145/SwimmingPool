0058706cf9ac3c2fbada223562180a591cd607da
prateek145 <prateekk898@gmail.com>
2024-03-18 10:46:16 +0530

    local

--- routes/api.php
+++ routes/api.php
@@ -25,10 +25,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function(){
+// Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function(){
 
 
-});
+// });
 
 Route::group(['prefix' => 'v1'], function(){
     Route::resource('members', UserController::class);
