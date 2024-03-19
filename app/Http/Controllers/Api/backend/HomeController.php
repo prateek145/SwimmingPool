dc60d649ed9b2493bb029e26fe5a631d2725aebe
prateek145 <prateekk898@gmail.com>
2024-03-19 13:00:56 +0530

    local

--- app/Http/Controllers/Api/backend/HomeController.php
+++ app/Http/Controllers/Api/backend/HomeController.php
@@ -20,6 +20,7 @@ class HomeController extends ResponseController
     {
         try {
             $members = DB::table('users')
+                ->where('users.role', '!=', 'admin')
                 ->join('allocate_packages', 'users.id', 'allocate_packages.member_id')
                 ->join('member_slots', 'users.id', 'member_slots.member_id')
                 ->get();
